18d6436e27ad4a7b58653b7a8f508d1dfc90f63b
sougata <sougata@itobuz.in>
2022-03-10 15:08:49 +0530

    fixed semicolon migrations

--- api/migrations/20220310104927_add-companies-table.js
+++ api/migrations/20220310104927_add-companies-table.js
@@ -1,5 +1,4 @@
-
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable('companies', (t) => {
     t.increments('id');
     t.string('name').notNullable();
@@ -15,6 +14,6 @@ exports.up = function(knex) {
   });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable('companies');
 };
